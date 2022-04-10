9eb7bc37c71d3877c437922011e294c16e224762
Md. Rakib Mah <99381836+rakibraz43@users.noreply.github.com>
2022-04-10 22:16:41 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// swiper js code
+
+
+
+    var swiper = new Swiper('.swiper', {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+       
+
+    });
+
+
+  <!-- Initialize Swiper -->
+  <script type="module">
+    
+    var swiper = new Swiper('.swiper', {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  </script>
+
+
+
+
+
+
